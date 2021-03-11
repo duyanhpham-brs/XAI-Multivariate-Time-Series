52081d1e111f96ba1fec64ee064864284fa20c20
duyanhpham-brs <duy.pham@smail.inf.h-brs.de>
2021-03-11 15:32:16 +0100

    added DataLoader convert function

--- utils/datasets.py
+++ utils/datasets.py
@@ -2,6 +2,22 @@ from scipy.io import arff
 import pandas as pd
 import numpy as np
 import os
+import torch
+from torch.utils.data import DataLoader, Dataset
+
+class MTSDataset(Dataset):
+    def __init__(self, inp, out, time_length, feature_length):
+        self.input = inp
+        self.output = out
+        self.dims = time_length
+        self.channels = feature_length
+        
+    def __len__(self):
+        return len(self.input)
+    
+    def __getitem__(self, index):
+        image = (self.input[index]).reshape(1,self.dims,self.channels)
+        label = self.output[index]
 
 class DatasetLoader:
     def __init__(self, folder_path):
@@ -25,3 +41,29 @@ class DatasetLoader:
         test_X = np.array([pd.DataFrame(test_data.loc[i][0]).values for i in range(len(test_data))],dtype=np.float)
         test_y = np.array([int(float(test_data.loc[i][1])) for i in range(len(test_data))],dtype=np.float)
         return train_X, train_y, test_X, test_y
+
+    def get_torch_dataset_loader_auto(self, train_batch_size, test_batch_size):
+        X_train, y_train, X_test, y_test = self.load_to_nparray()
+        train_dataset = MTSDataset(X_train, y_train, X_train.shape[-1], X_train.shape[-2])
+        train_loader = DataLoader(train_dataset, batch_size=train_batch_size, shuffle=True)
+        test_dataset = MTSDataset(X_test, y_test, X_test.shape[-1], X_test.shape[-2])
+        test_loader = DataLoader(test_dataset, batch_size=test_batch_size, shuffle=False)
+        
+        dataloaders = {}
+        dataloaders['train'] = train_loader
+        dataloaders['val'] = test_loader
+
+        return dataloaders
+
+    def get_torch_dataset_loader_custom(self, time_length, feature_length, train_batch_size, test_batch_size):
+        X_train, y_train, X_test, y_test = self.load_to_nparray()
+        train_dataset = MTSDataset(X_train, y_train, time_length, feature_length)
+        train_loader = DataLoader(train_dataset, batch_size=train_batch_size, shuffle=True)
+        test_dataset = MTSDataset(X_test, y_test, time_length, feature_length)
+        test_loader = DataLoader(test_dataset, batch_size=test_batch_size, shuffle=False)
+        
+        dataloaders = {}
+        dataloaders['train'] = train_loader
+        dataloaders['val'] = test_loader
+
+        return dataloaders
