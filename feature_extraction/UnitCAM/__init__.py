fc2ce1d9a2ba1329af8e1b292924715485910e1b
a11to1n3 <44360452+a11to1n3@users.noreply.github.com>
2021-04-15 08:40:26 +0700

    [MAINTAIN] Tested and documented Ablation-CAM (#26)
    Co-authored-by: duyanhpham-brs <duy.pham@smail.inf.h-brs.de>

--- feature_extraction/UnitCAM/__init__.py
+++ feature_extraction/UnitCAM/__init__.py
@@ -43,7 +43,7 @@ class UnitCAM:
         """
         return self.model(input_features)
 
-    def extract_features(self, input_features, print_out, index, zero_out=False):
+    def extract_features(self, input_features, print_out, index, zero_out=None):
         """Extract the feature maps of the targeted layer
 
         Attributes:
