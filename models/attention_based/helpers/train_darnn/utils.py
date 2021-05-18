9e4cc28af3457ec796b0fa5da2f0d0f07692a190
duyanhpham-brs <duy.pham@smail.inf.h-brs.de>
2021-05-18 22:42:19 +0200

    finished explaining RETAIN

--- models/attention_based/helpers/train_darnn/utils.py
+++ models/attention_based/helpers/train_darnn/utils.py
@@ -4,5 +4,5 @@ from torch.autograd import Variable
 from models.attention_based.helpers.train_darnn.constants import device
 
 
-def numpy_to_tvar(x: torch.Tensor):
+def numpy_to_tvar(x):
     return Variable(torch.from_numpy(x).type(torch.FloatTensor).to(device))
