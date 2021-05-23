c61e4a8867bfb898caec59f1c1e07d0e4dbf523f
duyanhpham-brs <duy.pham@smail.inf.h-brs.de>
2021-05-23 20:13:45 +0200

    fix the View layer dimension

--- models/CAM_based/MTEX/__init__.py
+++ models/CAM_based/MTEX/__init__.py
@@ -30,7 +30,7 @@ class MTEX(nn.Module):
                     ("relu_2", nn.ReLU(inplace=True)),
                     ("conv_3", nn.Conv2d(32, 1, 1)),
                     ("relu_3", nn.ReLU(inplace=True)),
-                    ("view", View((feature_length, time_length // 4 + 2))),
+                    ("view", View((feature_length, (time_length + 1)// 4 ))),
                     ("conv_4", nn.Conv1d(feature_length, 64, 3)),
                     ("relu_4", nn.ReLU(inplace=True)),
                 ]
