c2b557fb05b721576ffce59ef6b6214b78c92598
duyanhpham-brs <duy.pham@smail.inf.h-brs.de>
2021-08-03 22:39:01 +0200

    changed TXCM

--- models/CAM_based/TXCM/__init__.py
+++ models/CAM_based/TXCM/__init__.py
@@ -42,35 +42,28 @@ class TXCM(nn.Module):
             )
         )
 
-        self.main_batch_norm = nn.Sequential(
-            OrderedDict([("batchnorm_30", nn.BatchNorm2d(32))])
-        )
-
         self.cnn_layers3 = nn.Sequential(
             OrderedDict(
                 [
-                    ("swap_31", SwapLastDims()),
                     (
                         "conv_31",
-                        nn.Conv1d(
-                            time_length, 32, window_size, padding=window_size // 2
-                        ),
+                        nn.Conv2d(1, 16, 1),
                     ),
-                    ("batchnorm_31", nn.BatchNorm1d(32)),
-                    ("relu_3", nn.ReLU(inplace=True)),
+                    ("batchnorm_31", nn.BatchNorm2d(16)),
+                    ("relu_31", nn.ReLU(inplace=True)),
                 ]
             )
         )
 
         self.avgpool_layer = nn.Sequential(
-            OrderedDict([("avgpool", nn.AvgPool1d(feature_length))])
+            OrderedDict([("avgpool", nn.AvgPool2d((1, time_length)))])
         )
 
         self.linear_layers = nn.Sequential(
             OrderedDict(
                 [
                     ("flatten", nn.Flatten()),
-                    ("fc1", nn.Linear(32, n_classes)),
+                    ("fc1", nn.Linear(feature_length * 16, n_classes)),
                     ("softmax", nn.Softmax(dim=1)),
                 ]
             )
@@ -79,13 +72,11 @@ class TXCM(nn.Module):
     def forward(self, x):
         # 2d (spatial) branch
         first_branch = self.cnn_layers1_b1(x)
-        print(first_branch.shape)
         # 1d (temporal) branch
         second_branch = self.cnn_layers2_b1(x)
-        print(second_branch.shape)
         # Multiplication
         main_branch = first_branch * second_branch
-        main_branch = self.cnn_layers3(main_branch)
+        main_branch = self.cnn_layers3(main_branch.unsqueeze(1))
         main_branch = self.avgpool_layer(main_branch)
         main_branch = self.linear_layers(main_branch)
 
