fixed gradient extraction

models/CAM_based/TXCM/__init__.py
@@ -42,28 +42,31 @@ class TXCM(nn.Module):
             )
         )
 
-        self.cnn_layers3 = nn.Sequential(
+        self.cnn_layers3_txcm = nn.Sequential(
             OrderedDict(
                 [
+                    ("swap_31", SwapLastDims()),
                     (
                         "conv_31",
-                        nn.Conv2d(1, 16, 1),
+                        nn.Conv1d(
+                            time_length, 32, window_size, padding=window_size // 2
+                        ),
                     ),
-                    ("batchnorm_31", nn.BatchNorm2d(16)),
-                    ("relu_31", nn.ReLU(inplace=True)),
+                    ("batchnorm_31", nn.BatchNorm1d(32)),
+                    ("relu_3", nn.ReLU(inplace=True)),
                 ]
             )
         )
 
         self.avgpool_layer = nn.Sequential(
-            OrderedDict([("avgpool", nn.AvgPool2d((1, time_length)))])
+            OrderedDict([("avgpool", nn.AvgPool1d(feature_length))])
         )
 
         self.linear_layers = nn.Sequential(
             OrderedDict(
                 [
                     ("flatten", nn.Flatten()),
-                    ("fc1", nn.Linear(feature_length * 16, n_classes)),
+                    ("fc1", nn.Linear(32, n_classes)),
                     ("softmax", nn.Softmax(dim=1)),
                 ]
             )
@@ -76,7 +79,7 @@ class TXCM(nn.Module):
         second_branch = self.cnn_layers2_b1(x)
         # Multiplication
         main_branch = first_branch * second_branch
-        main_branch = self.cnn_layers3(main_branch.unsqueeze(1))
+        main_branch = self.cnn_layers3_txcm(main_branch)
         main_branch = self.avgpool_layer(main_branch)
         main_branch = self.linear_layers(main_branch)
 

utils/gradient_extraction/__init__.py
@@ -89,7 +89,28 @@ class ModelOutputs:
                 x = module(x)
                 x = x.view(x.size(0), -1)
             elif name.lower().find("linear") == -1:
-                x = torch.cat(tuple(branches[num_branch]), 1)
+                print(branches[num_branch][0].shape, branches[num_branch][1].shape)
+                if name == "cnn_layers3_txcm":
+                    x = (
+                        branches[num_branch][0].reshape(
+                            branches[num_branch][1].shape[0],
+                            -1,
+                            branches[num_branch][1].shape[2],
+                        )
+                        * branches[num_branch][1]
+                    )
+                else:
+                    x = torch.cat(
+                        (
+                            branches[num_branch][0].reshape(
+                                branches[num_branch][1].shape[0],
+                                -1,
+                                branches[num_branch][1].shape[2],
+                            ),
+                            branches[num_branch][1],
+                        ),
+                        1,
+                    )
                 x = module(x)
             elif name.lower().find("linear") != -1:
                 if len(x.size()) == 3: