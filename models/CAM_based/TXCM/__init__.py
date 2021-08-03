65fcaec2089eb64aff1c40e98c15fe07b87de90f
duyanhpham-brs <duy.pham@smail.inf.h-brs.de>
2021-08-03 20:23:41 +0200

    added TXCM

---
+++ models/CAM_based/TXCM/__init__.py
@@ -0,0 +1,92 @@
+from collections import OrderedDict
+import torch
+import torch.nn as nn
+from utils.training_helpers import View, Squeeze, SwapLastDims
+
+
+class TXCM(nn.Module):
+    def __init__(self, window_size, time_length, feature_length, n_classes):
+        super().__init__()
+        self.cnn_layers1_b1 = nn.Sequential(
+            OrderedDict(
+                [
+                    (
+                        "conv_11",
+                        nn.Conv2d(1, 16, window_size, padding=window_size // 2),
+                    ),
+                    ("batchnorm_11", nn.BatchNorm2d(16)),
+                    ("relu_11", nn.ReLU(inplace=True)),
+                    ("conv_12", nn.Conv2d(16, 1, 1)),
+                    ("relu_12", nn.ReLU(inplace=True)),
+                    ("squeeze_12", Squeeze()),
+                    ("swap_12", SwapLastDims()),
+                ]
+            )
+        )
+
+        self.cnn_layers2_b1 = nn.Sequential(
+            OrderedDict(
+                [
+                    ("view_21", View((feature_length))),
+                    (
+                        "conv_21",
+                        nn.Conv1d(
+                            feature_length, 16, window_size, padding=window_size // 2
+                        ),
+                    ),
+                    ("batchnorm_21", nn.BatchNorm1d(16)),
+                    ("relu_21", nn.ReLU(inplace=True)),
+                    ("conv_22", nn.Conv1d(16, 1, 1)),
+                    ("relu_22", nn.ReLU(inplace=True)),
+                ]
+            )
+        )
+
+        self.main_batch_norm = nn.Sequential(
+            OrderedDict([("batchnorm_30", nn.BatchNorm2d(32))])
+        )
+
+        self.cnn_layers3 = nn.Sequential(
+            OrderedDict(
+                [
+                    ("swap_31", SwapLastDims()),
+                    (
+                        "conv_31",
+                        nn.Conv1d(
+                            time_length, 32, window_size, padding=window_size // 2
+                        ),
+                    ),
+                    ("batchnorm_31", nn.BatchNorm1d(32)),
+                    ("relu_3", nn.ReLU(inplace=True)),
+                ]
+            )
+        )
+
+        self.avgpool_layer = nn.Sequential(
+            OrderedDict([("avgpool", nn.AvgPool1d(feature_length))])
+        )
+
+        self.linear_layers = nn.Sequential(
+            OrderedDict(
+                [
+                    ("flatten", nn.Flatten()),
+                    ("fc1", nn.Linear(32, n_classes)),
+                    ("softmax", nn.Softmax(dim=1)),
+                ]
+            )
+        )
+
+    def forward(self, x):
+        # 2d (spatial) branch
+        first_branch = self.cnn_layers1_b1(x)
+        print(first_branch.shape)
+        # 1d (temporal) branch
+        second_branch = self.cnn_layers2_b1(x)
+        print(second_branch.shape)
+        # Multiplication
+        main_branch = first_branch * second_branch
+        main_branch = self.cnn_layers3(main_branch)
+        main_branch = self.avgpool_layer(main_branch)
+        main_branch = self.linear_layers(main_branch)
+
+        return main_branch
